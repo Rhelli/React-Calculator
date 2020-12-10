e8de273e141972939a9cb651bc5373f51e91e83a
Rory Hellier <rory.hellier@googlemail.com>
2020-12-10 20:22:48 +0000

    Replaced manually entered buttons in ButtonPanel with individual uses of the Button component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ButtonPanel from 'ButtonPanel';
 
 const Display = props => {
   const value = { props };
   return (
     <div>
       <h3>{value}</h3>
+      <ButtonPanel />
     </div>
   );
 };
@@ -15,5 +17,5 @@ Display.propTypes = {
 };
 
 Display.defaultProps = {
-  value: 'something',
+  value: '0',
 };
