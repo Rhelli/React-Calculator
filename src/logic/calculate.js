b4fedf137bcbb2a33667134a154d3e0377c168b0
Rory Hellier <rory.hellier@googlemail.com>
2020-12-11 17:21:36 +0000

    Created initial build of the calculate sheet.

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,11 +1,57 @@
+import Operate from './operate';
+
 const calculate = (dataObject, buttonName) => {
   const { total, next, operation } = dataObject;
 
-  if (buttonName === '+/-') {
-    return {
-      total,
-      next,
-      operation
-    }
+  switch (buttonName) {
+    case '+/-':
+      if (operation) {
+        return {
+          total,
+          next: Operate(next, -1, 'x'),
+          operate,
+        }
+      }
+      return {
+        total: Operate(total, -1, 'x'),
+        next,
+        operate,
+      }
+
+    case '%':
+      if (operation) {
+        let preCalc = Operate(total, next, operation);
+        return {
+          total: Operate(preCalc, 100, '÷'),
+          next,
+          operation
+        }
+      }
+      return {
+        total: Operate(total, 100, '÷'),
+        next,
+        operation
+      }
+
+    case '=':
+      if (operation) {
+        return {
+          total: Operate(total, next, operation),
+          next,
+          operation: null
+        }
+      }
+      return {
+        total,
+        next,
+        operation
+      }
+
+    case 'AC':
+      return {
+        total: null,
+        next: null,
+        operation: null
+      }
   }
 }
