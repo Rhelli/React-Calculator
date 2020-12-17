03419e8214a2c3c0f79e4fa33c8e4a84d894d9eb
Rory Hellier <rory.hellier@googlemail.com>
2020-12-17 22:38:16 +0000

    Modified app.js more. Removed the class contructor, removed the incorrect function argument, amended the incorrect useState arguments

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 const App = () => {
-  //constructor(props) {
-  //  super(props);
-//
-  //  this.state = {
-  //    total: null,
-  //    next: null,
-  //    operation: null,
-  //  };
-//
-  //  this.handleClick = this.handleClick.bind(this);
-  //}
-
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
   const [operation, setOperation] = useState(null);
-  const [handleClickState, setHandleClickState] = useState(calculate(this.state, buttonName))
 
-  const handleClick = (buttonName) => {
+  handleClick(buttonName) {
     const value = calculate(this.state, buttonName);
-    setHandleClickState(value);
+    this.setState(value);
   }
 
-  useEffect(() => {
+  render() {
+    const { total, next, operation } = this.state;
     let toRender = '';
     if (operation && !next) { toRender = total; }
     if (operation && next) { toRender = next; }
     if (!operation) { toRender = total; }
-    return toRender;
-  }, [total, next, operation]);
-
-  return (
-    <div>
-      <Display value={toRender} />
-      <ButtonPanel clickHandler={handleClick} />
-    </div>
-  );
+    return (
+      <div>
+        <Display value={toRender} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
