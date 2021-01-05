3417a18e05f17bf58049d08b0856ff281508fdd2
Rory Hellier <rory.hellier@googlemail.com>
2021-01-05 22:41:49 +0000

    New styling changes and conditional class implementation:
     - Conditionally added  button colors depending on if they are operators or normal number buttons
     - Modified the colors for normal buttons
     - Made the zero button 50% width
     - Sized the rest of the buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -14,21 +14,22 @@ const ButtonPanel = props => {
   const group4 = ['1', '2', '3', '+'];
   const group5 = ['0', '.', '='];
 
+
   return (
     <div className={styles.buttonPanel}>
-      <div id="group1">
+      <div id="group1" className={styles.row}>
         { group1.map(val => <Button clickHandler={handleClick} key={`btn_${val}`} buttonName={val} />) }
       </div>
-      <div id="group2">
+      <div id="group2" className={styles.row}>
         {group2.map(val => <Button clickHandler={handleClick} key={`btn_${val}`} buttonName={val} />)}
       </div>
-      <div id="group3">
+      <div id="group3" className={styles.row}>
         {group3.map(val => <Button clickHandler={handleClick} key={`btn_${val}`} buttonName={val} />)}
       </div>
-      <div id="group4">
+      <div id="group4" className={styles.row}>
         {group4.map(val => <Button clickHandler={handleClick} key={`btn_${val}`} buttonName={val} />)}
       </div>
-      <div id="group5">
+      <div id="group5" className={styles.row}>
         {group5.map(val => <Button clickHandler={handleClick} key={`btn_${val}`} buttonName={val} />)}
       </div>
     </div>
