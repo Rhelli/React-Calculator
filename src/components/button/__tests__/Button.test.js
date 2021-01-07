54f6aafdb2adddf49444917a926810679ccadc4b
Rory Hellier <rory.hellier@googlemail.com>
2021-01-07 21:11:17 +0000

    Added tests folder to the button component folder and defined inline and snapshot tests. Both now passing

---
+++ src/components/button/__tests__/Button.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import Button from '../Button';
+
+describe('Button appearance', () => {
+  it('Matches the Button Snapshot', () => {
+    const tree = renderer.create(<Button />).toJSON()
+    expect(tree).toMatchSnapshot()
+  });
+
+  it('Matches its inline snapshot', () => {
+    const { container } = render(<Button />);
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        class="button"
+      >
+        <button
+          class="grey"
+          id=""
+          type="button"
+        />
+      </div>
+    `)
+  });
+});
