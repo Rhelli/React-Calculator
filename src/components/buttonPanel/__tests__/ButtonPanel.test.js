875bba83154df8dff0c5db302209fd222e463859
Rory Hellier <rory.hellier@googlemail.com>
2021-01-07 21:32:56 +0000

    Completed adding the button panel tests. Tested for matching snapshot and the contained button text in all buttons

---
+++ src/components/buttonPanel/__tests__/ButtonPanel.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import ButtonPanel from '../ButtonPanel';
+
+describe('The Button Panel Appearance', () => {
+  it('Matches the Button Panel Snapshot', () => {
+    const tree = renderer.create(<ButtonPanel />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders all of the buttons correctly', () => {
+    const { getByText } = render(<ButtonPanel />);
+
+    getByText('AC');
+    getByText('+/-');
+    getByText('%');
+    getByText('÷');
+    getByText('7');
+    getByText('8');
+    getByText('9');
+    getByText('x');
+    getByText('4');
+    getByText('5');
+    getByText('6');
+    getByText('-');
+    getByText('1');
+    getByText('2');
+    getByText('3');
+    getByText('+');
+    getByText('0');
+    getByText('.');
+    getByText('=');
+  })
+})
